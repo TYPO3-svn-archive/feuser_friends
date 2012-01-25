34e34f3775d4bbd41dd0484cbfa182e8b42a27b4
juergen.furrer <juergen.furrer@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-01-25 22:58:36 +0000

    Display multiple images (#33102) / Render rteFields (#33103) / Add jQuery UI 1.8.17
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/feuser_friends/trunk@56705 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_feuserfriends_pi1.php
+++ pi1/class.tx_feuserfriends_pi1.php
@@ -28,6 +28,7 @@
  */
 
 require_once(PATH_tslib.'class.tslib_pibase.php');
+require_once(t3lib_extMgm::extPath('feuser_friends').'lib/class.tx_feuserfriends_pagerenderer.php');
 
 /**
  * Plugin 'Frontend-user friends' for the 'feuser_friends' extension.
@@ -38,9 +39,9 @@ require_once(PATH_tslib.'class.tslib_pibase.php');
  */
 class tx_feuserfriends_pi1 extends tslib_pibase
 {
-	var $prefixId      = 'tx_feuserfriends_pi1';		// Same as class name
-	var $scriptRelPath = 'pi1/class.tx_feuserfriends_pi1.php';	// Path to this script relative to the extension dir.
-	var $extKey        = 'feuser_friends';	// The extension key.
+	var $prefixId      = 'tx_feuserfriends_pi1';
+	var $scriptRelPath = 'pi1/class.tx_feuserfriends_pi1.php';
+	var $extKey        = 'feuser_friends';
 	var $pi_checkCHash = true;
 	var $detailId      = null;
 	var $templateFile  = null;
@@ -54,7 +55,10 @@ class tx_feuserfriends_pi1 extends tslib_pibase
 	var $feuserId      = null;
 	var $friends_array = null;
 	var $piFlexForm = array();
-
+	var $img_fields = array();
+	var $url_fields = array();
+	var $rte_fields = array();
+	
 	/**
 	 * The main method of the PlugIn
 	 *
@@ -65,6 +69,10 @@ class tx_feuserfriends_pi1 extends tslib_pibase
 	function main($content, $conf)
 	{
 		$this->conf = $conf;
+
+		$this->pagerenderer = t3lib_div::makeInstance('tx_feuserfriends_pagerenderer');
+		$this->pagerenderer->setConf($this->conf);
+
 		$this->pi_USER_INT_obj = 1;
 		$this->pi_setPiVarDefaults();
 		$this->pi_loadLL();
@@ -74,8 +82,9 @@ class tx_feuserfriends_pi1 extends tslib_pibase
 		$this->internal['currentTable'] = 'fe_users';
 		$GLOBALS["TSFE"]->set_no_cache();
 		// 
-		$this->img_fields = t3lib_div::trimExplode(',', $this->conf['imageFields'], 1);
-		$this->url_fields = t3lib_div::trimExplode(',', $this->conf['linkFields'], 1);
+		$this->img_fields = t3lib_div::trimExplode(',', $this->conf['imageFields'], TRUE);
+		$this->url_fields = t3lib_div::trimExplode(',', $this->conf['linkFields'], TRUE);
+		$this->rte_fields = t3lib_div::trimExplode(',', $this->conf['rteFields'], TRUE);
 		// define the id for detaile
 		$this->detailId = (is_numeric($this->conf['detailId']) ? $this->conf['detailId'] : $GLOBALS['TSFE']->id);
 		// define the loggedin feuser
@@ -144,8 +153,18 @@ class tx_feuserfriends_pi1 extends tslib_pibase
 			}
 		}
 		$this->pi_addJS();
-		$this->addCssFile($this->conf['jQueryUIstyle']);
-		$this->addResources();
+		$this->pagerenderer->addCssFile($this->conf['jQueryUIstyle']);
+
+		// checks if t3jquery is loaded
+		if (T3JQUERY === true) {
+			tx_t3jquery::addJqJS();
+		} else {
+			$this->pagerenderer->addJsFile($this->conf['jQueryLibrary'], true);
+			$this->pagerenderer->addJsFile($this->conf['jQueryEasing']);
+			$this->pagerenderer->addJsFile($this->conf['jQueryUI']);
+		}
+
+		$this->pagerenderer->addResources();
 
 		return $this->pi_wrapInBaseClass($content);
 	}
@@ -825,6 +844,7 @@ class tx_feuserfriends_pi1 extends tslib_pibase
 				break;
 			default:
 				if (in_array($fN, $this->img_fields)) {
+					// Image fields
 					$img_conf = $this->conf[$this->mode]['imgWraps.']["$fN."];
 					if (!is_array($img_conf)) {
 						$img_conf = array();
@@ -835,10 +855,16 @@ class tx_feuserfriends_pi1 extends tslib_pibase
 						$img_dir = $GLOBALS['TCA'][$this->internal['currentTable']]['columns'][$fN]['config']['uploadfolder'];
 					}
 					$img_dir .= '/';
-					list($img) = explode(',',$this->internal['currentRow'][$fN]);
-					$img_conf['file'] = $img_dir.$img;
-					$content = $this->cObj->IMAGE($img_conf);
+					$imgages = t3lib_div::trimExplode(",", $this->internal['currentRow'][$fN]);
+					foreach ($imgages as $img) {
+						$img_conf['file'] = $img_dir.$img;
+						$content .= $this->cObj->IMAGE($img_conf);
+					}
+				} elseif (in_array($fN, $this->rte_fields)) {
+					// RTE fields
+					$content = $this->pi_RTEcssText($this->internal['currentRow'][$fN]);
 				} elseif (in_array($fN, $this->url_fields)) {
+					// URL fields
 					$link_conf = $this->conf[$this->mode]['linkWraps.']["$fN."];
 					if (!is_array($link_conf))	$link_conf = array();
 					$link_conf['parameter'] = $this->internal['currentRow'][$fN];
@@ -899,7 +925,7 @@ class tx_feuserfriends_pi1 extends tslib_pibase
 	 */
 	function pi_addJS()
 	{
-		$this->addJS("
+		$this->pagerenderer->addJS("
 var feuser_friends = {
 	dialogToAdd: function(uid) {
 		jQuery('#dialog').remove();
@@ -1043,213 +1069,6 @@ var feuser_friends = {
 		});
 	}
 };");
-	}
-
-	
-	/**
-	 * Include all defined resources (JS / CSS)
-	 *
-	 * @return void
-	 */
-	function addResources()
-	{
-		// checks if t3jquery is loaded
-		if (T3JQUERY === true) {
-			tx_t3jquery::addJqJS();
-		} else {
-			$this->addJsFile($this->conf['jQueryLibrary'], true);
-			$this->addJsFile($this->conf['jQueryEasing']);
-			$this->addJsFile($this->conf['jQueryUI']);
-		}
-		if (t3lib_div::int_from_ver(TYPO3_version) >= 4003000) {
-			$pagerender = $GLOBALS['TSFE']->getPageRenderer();
-		}
-		// Fix moveJsFromHeaderToFooter (add all scripts to the footer)
-		if ($GLOBALS['TSFE']->config['config']['moveJsFromHeaderToFooter']) {
-			$allJsInFooter = true;
-		} else {
-			$allJsInFooter = false;
-		}
-		// add all defined JS files
-		if (count($this->jsFiles) > 0) {
-			foreach ($this->jsFiles as $jsToLoad) {
-				if (T3JQUERY === true) {
-					$conf = array(
-						'jsfile' => $jsToLoad,
-						'tofooter' => ($this->conf['jsInFooter'] || $allJsInFooter),
-						'jsminify' => $this->conf['jsMinify'],
-					);
-					tx_t3jquery::addJS('', $conf);
-				} else {
-					$file = $this->getPath($jsToLoad);
-					if ($file) {
-						if (t3lib_div::int_from_ver(TYPO3_version) >= 4003000) {
-							if ($this->conf['jsInFooter'] || $allJsInFooter) {
-								$pagerender->addJsFooterFile($file, 'text/javascript', $this->conf['jsMinify']);
-							} else {
-								$pagerender->addJsFile($file, 'text/javascript', $this->conf['jsMinify']);
-							}
-						} else {
-							$temp_file = '<script type="text/javascript" src="'.$file.'"></script>';
-							if ($this->conf['jsInFooter'] || $allJsInFooter) {
-								$GLOBALS['TSFE']->additionalFooterData['jsFile_'.$this->extKey.'_'.$file] = $temp_file;
-							} else {
-								$GLOBALS['TSFE']->additionalHeaderData['jsFile_'.$this->extKey.'_'.$file] = $temp_file;
-							}
-						}
-					} else {
-						t3lib_div::devLog("'{$jsToLoad}' does not exists!", $this->extKey, 2);
-					}
-				}
-			}
-		}
-		// add all defined JS script
-		if (count($this->js) > 0) {
-			foreach ($this->js as $jsToPut) {
-				$temp_js .= $jsToPut;
-			}
-			$conf = array();
-			$conf['jsdata'] = $temp_js;
-			if (T3JQUERY === true && t3lib_div::int_from_ver($this->getExtensionVersion('t3jquery')) >= 1002000) {
-				$conf['tofooter'] = ($this->conf['jsInFooter'] || $allJsInFooter);
-				$conf['jsminify'] = $this->conf['jsMinify'];
-				$conf['jsinline'] = $this->conf['jsInline'];
-				tx_t3jquery::addJS('', $conf);
-			} else {
-				// Add script only once
-				$hash = md5($temp_js);
-				if ($this->conf['jsInline']) {
-					$GLOBALS['TSFE']->inlineJS[$hash] = $temp_js;
-				} elseif (t3lib_div::int_from_ver(TYPO3_version) >= 4003000) {
-					if ($this->conf['jsInFooter'] || $allJsInFooter) {
-						$pagerender->addJsFooterInlineCode($hash, $temp_js, $this->conf['jsMinify']);
-					} else {
-						$pagerender->addJsInlineCode($hash, $temp_js, $this->conf['jsMinify']);
-					}
-				} else {
-					if ($this->conf['jsMinify']) {
-						$temp_js = t3lib_div::minifyJavaScript($temp_js);
-					}
-					if ($this->conf['jsInFooter'] || $allJsInFooter) {
-						$GLOBALS['TSFE']->additionalFooterData['js_'.$this->extKey.'_'.$hash] = t3lib_div::wrapJS($temp_js, true);
-					} else {
-						$GLOBALS['TSFE']->additionalHeaderData['js_'.$this->extKey.'_'.$hash] = t3lib_div::wrapJS($temp_js, true);
-					}
-				}
-			}
-		}
-		// add all defined CSS files
-		if (count($this->cssFiles) > 0) {
-			foreach ($this->cssFiles as $cssToLoad) {
-				// Add script only once
-				$file = $this->getPath($cssToLoad);
-				if ($file) {
-					if (t3lib_div::int_from_ver(TYPO3_version) >= 4003000) {
-						$pagerender->addCssFile($file, 'stylesheet', 'all', '', $this->conf['cssMinify']);
-					} else {
-						$GLOBALS['TSFE']->additionalHeaderData['cssFile_'.$this->extKey.'_'.$file] = '<link rel="stylesheet" type="text/css" href="'.$file.'" media="all" />'.chr(10);
-					}
-				} else {
-					t3lib_div::devLog("'{$cssToLoad}' does not exists!", $this->extKey, 2);
-				}
-			}
-		}
-		// add all defined CSS Script
-		if (count($this->css) > 0) {
-			foreach ($this->css as $cssToPut) {
-				$temp_css .= $cssToPut;
-			}
-			$hash = md5($temp_css);
-			if (t3lib_div::int_from_ver(TYPO3_version) >= 4003000) {
-				$pagerender->addCssInlineBlock($hash, $temp_css, $this->conf['cssMinify']);
-			} else {
-				// addCssInlineBlock
-				$GLOBALS['TSFE']->additionalCSS['css_'.$this->extKey.'_'.$hash] .= $temp_css;
-			}
-		}
-	}
-
-	/**
-	 * Return the webbased path
-	 * 
-	 * @param string $path
-	 * return string
-	 */
-	function getPath($path="")
-	{
-		return $GLOBALS['TSFE']->tmpl->getFileName($path);
-	}
-
-	/**
-	 * Add additional JS file
-	 * 
-	 * @param string $script
-	 * @param boolean $first
-	 * @return void
-	 */
-	function addJsFile($script="", $first=false)
-	{
-		if ($this->getPath($script) && ! in_array($script, $this->jsFiles)) {
-			if ($first === true) {
-				$this->jsFiles = array_merge(array($script), $this->jsFiles);
-			} else {
-				$this->jsFiles[] = $script;
-			}
-		}
-	}
-
-	/**
-	 * Add JS to header
-	 * 
-	 * @param string $script
-	 * @return void
-	 */
-	function addJS($script="")
-	{
-		if (! in_array($script, $this->js)) {
-			$this->js[] = $script;
-		}
-	}
-
-	/**
-	 * Add additional CSS file
-	 * 
-	 * @param string $script
-	 * @return void
-	 */
-	function addCssFile($script="")
-	{
-		if ($this->getPath($script) && ! in_array($script, $this->cssFiles)) {
-			$this->cssFiles[] = $script;
-		}
-	}
-
-	/**
-	 * Add CSS to header
-	 * 
-	 * @param string $script
-	 * @return void
-	 */
-	function addCSS($script="")
-	{
-		if (! in_array($script, $this->css)) {
-			$this->css[] = $script;
-		}
-	}
-
-	/**
-	 * Returns the version of an extension (in 4.4 its possible to this with t3lib_extMgm::getExtensionVersion)
-	 * @param string $key
-	 * @return string
-	 */
-	function getExtensionVersion($key)
-	{
-		if (! t3lib_extMgm::isLoaded($key)) {
-			return '';
-		}
-		$_EXTKEY = $key;
-		include(t3lib_extMgm::extPath($key) . 'ext_emconf.php');
-		return $EM_CONF[$key]['version'];
 	}
 
 	/**
